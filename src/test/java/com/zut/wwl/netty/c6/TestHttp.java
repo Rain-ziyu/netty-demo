8a9a17944a4a433f9d3534878aec0fdfb652a395
wwl <wwl@test.com>
2021-12-21 16:21:25 +0800

    解析Http

--- src/test/java/com/zut/wwl/netty/c6/TestHttp.java
+++ src/test/java/com/zut/wwl/netty/c6/TestHttp.java
@@ -1,17 +1,16 @@
 package com.zut.wwl.netty.c6;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.AdaptiveRecvByteBufAllocator;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.*;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
 
 @Slf4j
 public class TestHttp {
@@ -21,13 +20,38 @@ public class TestHttp {
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.channel(NioServerSocketChannel.class);
-            serverBootstrap.childOption(ChannelOption.RCVBUF_ALLOCATOR,new AdaptiveRecvByteBufAllocator(16,16,16));
             serverBootstrap.group(boss, worker);
             serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) {
                     ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                     ch.pipeline().addLast(new HttpServerCodec());
+/*                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            log.debug("{}",msg.getClass());
+                            if(msg instanceof HttpRequest){
+
+                            }else if (msg instanceof HttpContent){
+
+                            }
+                            super.channelRead(ctx, msg);
+                        }
+                    });*/
+//                    这样的话只会对符合HttpRequest类型的消息才会被该处理器处理
+                    ch.pipeline().addLast(new SimpleChannelInboundHandler<HttpRequest>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpRequest httpRequest) throws Exception {
+                          log.debug(httpRequest.uri());
+//                          返回响应
+                            DefaultFullHttpResponse response = new DefaultFullHttpResponse(httpRequest.protocolVersion(), HttpResponseStatus.OK);
+                            byte[] bytes = "<h1> hello,world<h1>".getBytes();
+                            response.headers().setInt(CONTENT_LENGTH,bytes.length);
+                            response.content().writeBytes(bytes);
+//                            写回相应
+                            channelHandlerContext.writeAndFlush(response);
+                        }
+                    });
                 }
             });
             ChannelFuture channelFuture = serverBootstrap.bind(8080);
